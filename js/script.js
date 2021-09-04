fc7e9fc4611bc82162923a1b7a4f45d95bbe6504
bazheno <bazheno@gmail.com>
2021-09-04 14:44:04 +0300

    films database first commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-console.log(1);
+const numberOfFillms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB =  {
+count: numberOfFillms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
+
+const a = prompt ("Один из последних просмотренных фильмов?", ""),
+      b = prompt ("На сколько оцените его?", ""),
+      c = prompt ("Один из последних просмотренных фильмов?", ""),
+      d = prompt ("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log (personalMovieDB);
 
